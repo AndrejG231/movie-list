fe141a00eaf038e95848a1a2e4f4f793d164c3e4
Andrej Germič <andrej.germic@moderntv.eu>
2022-09-26 19:49:20 +0200

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import "antd/dist/antd.min.css"
+import MovieListPage from "./pages/movie-list/MovieList"
+
+function App() {
+  return <MovieListPage />
+}
+
+export default App
---
+++ src/pages/movie-list/MovieList.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MovieListPage = () => {
+  return <div style={{ background: "red" }}>Hello</div>
+}
+
+export default MovieListPage
